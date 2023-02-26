cb926c99911bef29d61c885a3740d4a96058b05e
Ilya <ilya.kunin@mail.ru>
2023-02-26 22:20:58 +0300

    Added generation steps

--- Poker/urls.py
+++ Poker/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import index_page, generate_mode_page
+from app.views import index_page, generate_mode_page, test_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_page),
     path('generator/', generate_mode_page),
+    path('test/', test_page)
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from dataclasses import dataclass
 
 from django.shortcuts import render
@@ -10,6 +11,9 @@ from engine.responses.generator_response import GeneratorResponse
 from engine.toolkit.constants import *
 
 
+total_response: GeneratorResponse = None
+
+
 @dataclass
 class GeneratorContext:
     player: Player
@@ -18,19 +22,30 @@ class GeneratorContext:
     files: list[str]
 
 
-def index_page(request):
-    return render(request, "index.html")
-
-
 def get_card_file(card, combination):
     if card is None:
         return "jokers.png"
     return f"{card.rank}-{card.suit}{'-frame' if card in combination.cards else ''}.png"
 
 
+def index_page(request):
+    return render(request, "index.html")
+
+
 def generate_mode_page(request, stage = 0):
-    response: GeneratorResponse = Game().play()
+    global total_response
     template = loader.get_template('generator.html')
+
+    # request
+    if request.POST.get('prev'):
+        stage = int(request.POST.get('stage')) - 1
+    elif request.POST.get('next'):
+        stage = int(request.POST.get('stage')) + 1
+    if stage == 3 or (not request.POST.get('next') and not request.POST.get('prev')):
+        total_response = Game().play()
+        stage = 0
+    stage = max(min(stage, len(tours) - 1), 0)
+    response: GeneratorResponse = deepcopy(total_response)
 
     # context
     players_context = list()
@@ -41,8 +56,21 @@ def generate_mode_page(request, stage = 0):
     for player, prediction, combination in zip(response.players, response.predictions[stage], response.players_combinations[stage]):
         players_context.append(GeneratorContext(player, prediction, combination, [get_card_file(card, best_combination) for card in player.cards]))
 
-    context = {"best_combination": best_combination,
+    context = {"stage": stage,
                "players_context": players_context[:-1],
                "dealer_context": players_context[-1:]}
+
+    return HttpResponse(template.render(context, request))
+
+
+def test_page(request, current = 1):
+    template = loader.get_template('test.html')
+
+    if request.POST.get('prev'):
+        current = int(request.POST.get('current')) - 1
+    elif request.POST.get('next'):
+        current = int(request.POST.get('current')) + 1
+
+    context = {"current": current}
 
     return HttpResponse(template.render(context, request))
